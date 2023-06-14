c78d0883b4a9c91917b219190aab81154788356b
Simon Clark <simon.clark@sintef.no>
2023-06-14 22:29:44 +0200

    initial files

---
+++ participants_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+
+def main():
+    st.title("Hello, Streamlit!")
+    st.write("Welcome to Streamlit in Colab!")
+
+if __name__ == '__main__':
+    main()
